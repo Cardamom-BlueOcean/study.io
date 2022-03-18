b77057f383d466de35351b729ed67a849b71fe76
MiguelsPizza <alexmnahas@gmail.com>
2022-03-18 13:07:33 -0400

    added auth sign in

--- client/App.tsx
+++ client/App.tsx
@@ -7,10 +7,39 @@ import {
   ThemeProvider,
   Typography,
 } from "@mui/material";
+import firebase from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+import "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCi0_iWWgSu1O8yk_LCflEUTnlVUev3K8",
+  authDomain: "studyio-97c11.firebaseapp.com",
+  projectId: "studyio-97c11",
+  storageBucket: "studyio-97c11.appspot.com",
+  messagingSenderId: "776844865130",
+  appId: "1:776844865130:web:18af58299f13a63df0a413",
+  measurementId: "G-90FNB3EGMV"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const firestore = getFirestore();
+// const database = getDatabase(app);
 
 type Props = {};
 
 function App({}: Props) {
+
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
   const theme = createTheme({
     palette: {
       primary: {
@@ -41,7 +70,7 @@ function App({}: Props) {
           elevation={3}
           sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
         >
-          <Typography color="primary.dark" variant="h1">
+          <Typography color="primary.dark" variant="h1" onClick={()=> {signInWithGoogle()}}>
             Sign in With Google
           </Typography>
         </Paper>

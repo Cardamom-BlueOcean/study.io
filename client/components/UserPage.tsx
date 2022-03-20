be4961050339af41d9e839d8c82a7b77c19a532d
Richard Linley <rj.linley@gmail.com>
2022-03-20 12:17:02 -0400

    Moved some files around for a cleaner file explorer, messed with Redux Arrays a little

--- client/components/UserPage.tsx
+++ client/components/UserPage.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import GroupTabs from './GroupTabs';
-import ReduxExample from './reduxExample';
-import AutreRedux from './AutreRedux';
+import ReduxExample from './redux-examples/ReduxExample';
+import AutreRedux from './redux-examples/AutreRedux';
 //Redux Imports Below:
 import { Provider } from 'react-redux';
 import { store } from '../store';
@@ -13,12 +13,13 @@ export default function UserPage() {
 
 
   // Everything within the provider tags below will have access to our global redux variables.
+  // test
   return (
 
     <div>
       <GroupTabs />
-      {/* <ReduxExample />
-      <AutreRedux /> */}
+      <ReduxExample />
+      <AutreRedux />
 
     </div>
 

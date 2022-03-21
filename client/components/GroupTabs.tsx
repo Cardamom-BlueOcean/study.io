c79c552f84cbe809b60d05fab6b862d48bb326c2
MiguelsPizza <alexmnahas@gmail.com>
2022-03-21 17:12:06 -0400

    added redux variables updated by db Subscriptions to users and roooms

--- client/components/GroupTabs.tsx
+++ client/components/GroupTabs.tsx
@@ -1,23 +1,37 @@
-import * as React from 'react';
-import { Box, Tab } from '@mui/material';
-import { TabContext, TabList, TabPanel } from '@mui/lab';
+import * as React from "react";
+import { Box, Tab } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
 //import { fakeData } from './fakeGroupData';
-
-
-
+import { useAppSelector, useAppDispatch } from "../hooks";
 
 export default function GroupTabs() {
-  const [value, setValue] = React.useState<string>('1');
-  //console.log(fakeData);
+  const [value, setValue] = React.useState<string>("1");
+  const [roomInput, setRoomInput] = React.useState<string>("");
 
-  const handleChange = (event: any, newValue :string) => {
+  const handleChange = (event: any, newValue: string) => {
     setValue(newValue);
   };
 
+  const userRooms = useAppSelector((state) => state.userRooms.value);
+  const createRoomFunction = useAppSelector(
+    (state) => state.globalFunctions.value.createRoom
+  );
+  const dispatch = useAppDispatch();
+
+  const handleRoomInput = (value) => {
+    setRoomInput(value);
+  };
+
+  const handleCreateRoom = () => {
+    createRoomFunction(roomInput);
+  };
+
+  // console.log("I am in group tabs :)", userRooms);
+
   return (
-    <Box sx={{ width: '100%', typography: 'body1' }}>
+    <Box sx={{ width: "100%", typography: "body1" }}>
       <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
             <Tab label="Javascript" value="1" />
             <Tab label="Express" value="2" />
@@ -28,7 +42,14 @@ export default function GroupTabs() {
         <TabPanel value="2">Item Two</TabPanel>
         <TabPanel value="3">Item Three</TabPanel>
       </TabContext>
+      <input
+        id="test"
+        type="text"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+          handleRoomInput(e.target.value)
+        }
+      ></input>
+      <button onClick={handleCreateRoom}>Add New Room</button>
     </Box>
-
   );
 }
--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -7,11 +7,9 @@ import {
   ThemeProvider,
   Typography,
 } from "@mui/material";
-import firebase from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 import "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollectionData } from "react-firebase-hooks/firestore";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";

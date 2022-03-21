14fe287a0e0ae2c23ae8740dae4429188f63c8f9
Amy Shi <amyshi218@gmail.com>
2022-03-21 11:38:12 -0400

    imported mui box to calendar

--- client/components/Calendar.tsx
+++ client/components/Calendar.tsx
@@ -1,23 +1,30 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import CalendarModal from './CalendarModal';
+import {
+  Box,
+  CssBaseline,
+  Paper,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 
-const Reminder = styled.div`
-  height: 500px;
-  width: 300px;
-  border: 3px solid black;
-`
+// const Reminder = styled.div`
+//   height: 500px;
+//   width: 300px;
+//   border: 3px solid black;
+// `
 
 export default function Calendar() {
 
   const [openCalendarModal, setOpenCalendarModal] = React.useState<boolean>(false);
 
   return (
-    <Reminder>
+    <Box>
 
 
       {openCalendarModal}
       <CalendarModal />
-    </Reminder>
+    </Box>
   );
 }

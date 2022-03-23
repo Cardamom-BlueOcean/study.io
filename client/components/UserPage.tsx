5a02e16fb072a62a8e6914b39d90261782fcab56
MiguelsPizza <alexmnahas@gmail.com>
2022-03-23 11:57:41 -0400

    git push origin AlexBranch

--- client/components/UserPage.tsx
+++ client/components/UserPage.tsx
@@ -64,7 +64,7 @@ export default function UserPage() {
               acceptedInvites: [],
               pendingInvites: [],
               rooms: []
-            });
+            },{ merge: true });
           };
           addUserDbIfUserIsNotAlreadyAdded();
 
